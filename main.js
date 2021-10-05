d1efbdec36a6b72838bad3b2240789816caf6bc5
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-05 22:11:16 +0900

    Js : navbar auto active

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ navbarMenu.addEventListener('click',(event)=>{
     }
     navbarMenu.classList.remove('active')
     scrollIntoView(link)
+    selectNavItem(target);
 })
 
 // Navbar Toggle button active
@@ -143,4 +144,49 @@ function scrollIntoView(selector){
     contact.scrollIntoView({behavior : "smooth"});
 }
 
+// 1. 모든 섹션 요소들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
 
+const sectionIds = ['#home','#about','#skills','#work','#testimonials','#contact']
+const sections = sectionIds.map(id => document.querySelector(id))
+const navItems =sectionIds.map(id=>document.querySelector(`[data-link="${id}"]`))
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('active')
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active')
+}
+const observerOptions = {
+    root:null,
+    rootMargin:'0px',
+    threshold: 0.3,
+}
+const observerCallback = (entries, observer)=>{
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            if(entry.boundingClientRect.y < 0){
+                selectedNavIndex = index + 1
+            }else{
+                selectedNavIndex = index - 1
+            }
+        }
+        })
+}
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section))
+
+window.addEventListener('wheel',()=>{
+    if(window.scrollY === 0){
+        selectedNavIndex = 0;
+    } else if (
+        window.scrollY + window.innerHeight ===
+        document.body.clientHeight
+    ) {
+        selectedNavIndex = navItems.length -1;
+    }
+    selectNavItem(navItems[selectedNavIndex])
+})
